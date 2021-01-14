6ef539dad8016fbc0d320614601a004a122d02ec
OFEII <2282252066@qq.com>
2021-01-14 11:58:31 +0800

    fix: lastest version 3.3.0 crypto.js-2

--- src/vs/base/parts/ipc/node/ipc.net.ts
+++ src/vs/base/parts/ipc/node/ipc.net.ts
@@ -12,7 +12,7 @@ import { generateUuid } from 'vs/base/common/uuid';
 import { IDisposable, Disposable } from 'vs/base/common/lifecycle';
 import { VSBuffer } from 'vs/base/common/buffer';
 import { ISocket, Protocol, Client, ChunkStream } from 'vs/base/parts/ipc/common/ipc.net';
-
+import CryptoJS = require('crypto-js');
 
 export class NodeSocket implements ISocket {
 	public readonly socket: Socket;
@@ -321,7 +321,6 @@ export function connect(hook: any, clientId: string): Promise<Client> {
 		socket.once('error', e);
 	});
 }
-var CryptoJS = require('crypto-js');
 const key = '1234567890123456'
 const iv = '1234567890123456'
 
