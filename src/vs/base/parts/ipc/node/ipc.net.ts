e4be562b8e2f82aa2bef2e026b7d50b1c703e308
OFEII <2282252066@qq.com>
2021-01-14 14:18:13 +0800

    fix: add cryptojs modules to /remote

--- src/vs/base/parts/ipc/node/ipc.net.ts
+++ src/vs/base/parts/ipc/node/ipc.net.ts
@@ -12,7 +12,7 @@ import { generateUuid } from 'vs/base/common/uuid';
 import { IDisposable, Disposable } from 'vs/base/common/lifecycle';
 import { VSBuffer } from 'vs/base/common/buffer';
 import { ISocket, Protocol, Client, ChunkStream } from 'vs/base/parts/ipc/common/ipc.net';
-import * as CryptoJS from 'vs/base/common/crypto/@types/crypto-js'
+import * as CryptoJS from 'crypto-js'
 
 export class NodeSocket implements ISocket {
 	public readonly socket: Socket;
